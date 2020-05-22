0b7deb4cf52549a001dc13bfa1409a5313c060e8
Frerik Andriessen <f.d.andriessen@gmail.com>
2020-05-22 18:48:38 +0200

    Populated the homepage view

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
-        # result = resolve("admin/")
         result = resolve("/")
-
         self.assertEqual(result.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        req = HttpRequest()
+        response = home_page(req)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.endswith("</html>"))
