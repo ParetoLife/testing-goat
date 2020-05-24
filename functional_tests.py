b875a4b2c251bbec1d36f55178d141592c438dc9
Frerik Andriessen <f.d.andriessen@gmail.com>
2020-05-24 18:57:31 +0200

    Updated code guided by the black and flake8 checks

--- functional_tests.py
+++ functional_tests.py
@@ -15,19 +15,16 @@ class NewVisitorTest(unittest.TestCase):
 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
-        self.assertIn('To-Do', header_text)
-        
+        self.assertIn("To-Do", header_text)
+
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id("id_new_item")
-        self.assertEqual(
-            inputbox.get_attribute("placeholder"),
-            "Enter a to-do item"
-        )
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
@@ -40,12 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
 
-        self.assertTrue(
-            any(
-                row.text == "1: Buy peacock feathers" for row in rows
-            )
-        )
-
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
--- superlists/urls.py
+++ superlists/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from lists.views import home_page
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', home_page),
+    url(r"^admin/", admin.site.urls),
+    url(r"^$", home_page),
 ]
